2768ef47ab8b3844e3c51a2c132c6bc06f7f3957
AndreweighsaTon <85970233+AndreweighsaTon@users.noreply.github.com>
2021-07-11 18:23:30 -0700

    Update Maze.java

--- src/main/java/org/atdp/Maze.java
+++ src/main/java/org/atdp/Maze.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.Stack;
 
 /**
  * Contains the basic structure and utilities for generating mazes up to 100x100 in size.
@@ -34,18 +35,37 @@ public class Maze extends Graph {
      */
     public Maze(int height, int width, int seed) {
         // Call Graph constructor
+
         super(height * width);
 
+
         // Instantiate variables
+        this.height = height;
+        this.width = width;
+        gen = new Random(seed);
+
 
         // Generate maze
+
+        generateMaze();
+
+
+
+
+
+
     }
 
     /**
      * Creates a maze with a random seed from 0 to 32767.
      */
     public Maze(int height, int width) {
+
         this(height, width, (int)(Math.random() * 32768));
+
+
+
+
     }
 
     // Getter functions
@@ -65,6 +85,72 @@ public class Maze extends Graph {
      */
     void generateMaze() {
         // YOUR CODE HERE
+        boolean[] visited = new boolean[this.height * this.width]; // Creates a new boolean length of the width and height.
+        Stack<Vertex> newStack = new Stack<>(); // creates a new stack of Vertexes
+        Vertex vert = vertices.get(0); // Stores the first index of the given vertices
+        while (vert != vertices.get(this.height * this.width - 1)) { // While the current vert is not equal to the last vert, continue.
+            newStack.add(vert); // adds the vert to the stack
+            visited[vert.id] = true; // adds true to the boolean array because it has been visited.
+            while ((vert = addNextEdge(vert, visited)) == null) { //new added edge is the current vert
+                vert = newStack.pop(); // pop when all neighbors have been visited.
+            }
+            newStack.add(vert); // adds vert to stack
+            visited[vert.id] = true; // marks as visited
+        }
+        // Make sure all tiles are connected by checking if it's visited or not and random
+        // select a neighbor
+        for(int i = 0; i < this.height*this.width; i++) { // for all the vertices in maze
+            if(visited[i] == false) { // if it is not visited
+                vert = vertices.get(i); //scan for one that is not visited
+                List<Vertex> neighbors = getNeighbors(vert);// getting all the neighbors of the given vertex
+                int next = gen.nextInt(neighbors.size()); // choose next random neighbor
+                Vertex v = neighbors.get(next); // getting next neighbor
+                if(visited[v.id] == true) { // if has been visited
+                    addEdge(vert.id, v.id); // add new edge
+                    visited[vert.id] = true;
+                    continue;
+                }
+                else {
+                    for (Vertex x : neighbors) {
+                        if (visited[x.id] == true) {
+                            addEdge(vert.id, x.id);
+                            visited[vert.id] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+    Vertex addNextEdge(Vertex u, boolean visited[])
+    {
+        boolean isThere = false;
+        List<Vertex> neighbors = getNeighbors(u); //Gets the Neighbors of Given Vertex
+        int next = gen.nextInt(neighbors.size()); //generates the next id randomly
+
+        Vertex v = neighbors.get(next); // Stores the next id to go to in Vertex V
+        if(visited[v.id] == false) { // if it hasn't visited
+            isThere = true; //found a node to go to
+        }
+        else {
+
+            for (Vertex x : neighbors) {
+                if (visited[x.id] == false) { // if neighbor hasn't been visited
+                    v = x; // moving to a spot that has not been visited
+                    isThere = true; // found a neighbor that isn't visited
+                    break;
+                }
+            }
+        }
+        if(isThere == false) {
+            return null; // if nothing is found return null and now other method has to backtrack
+        }
+        visited[v.id] = true; // visit turns to true
+        addEdge(u.id, v.id); // creates and edge between the two.
+
+        return v; // moving to new neighbor
     }
 
     /**
@@ -77,11 +163,77 @@ public class Maze extends Graph {
      * but the neighbors of 6 are only [3, 7].
      */
     List<Vertex> getNeighbors(Vertex v) {
+
         List<Vertex> neighbors = new ArrayList<>();
         
         // YOUR CODE HERE
 
-        return neighbors;
+
+
+       // for(int i =0; i < capacity; i++){
+     //       neighbors.add(new Vertex(i));
+     //   }
+
+        List<Vertex> temp = new ArrayList<>();
+    /*
+        // check for x axis for cornering
+        // check for y axis for cornering
+        if((v.id % width != 0) && (v.id % width != (width-1)) && (v.id > width) && (v.id < width * (height - 1))){
+            temp.add(new Vertex(v.id-width));
+            temp.add(new Vertex (v.id-1));
+            temp.add(new Vertex (v.id+1));
+            temp.add(new Vertex(v.id+width));
+        }
+
+        // v.id == width-1 || v.id == width*2 || v.id == (width*height-1)
+
+        //Top Left corner
+        if(v.id==0){
+            temp.add(new Vertex(1));
+            temp.add(new Vertex(width));
+
+        }
+
+
+
+        //Left Bottom corner
+        if(v.id==width *(height-1)){
+            temp.add(new Vertex(v.id+1));
+            temp.add(new Vertex(v.id-1));
+        }
+
+        //Right Bottom  corner
+        if(v.id== width*height - 1){
+            temp.add(new Vertex(v.id-width));
+            temp.add(new Vertex(v.id-1));
+
+        }
+
+
+        */
+
+        //west
+        if(v.id % width != 0){
+            temp.add(new Vertex(v.id-1));  // left Node
+        }
+
+        if((v.id + 1) % width != 0){
+            temp.add(new Vertex(v.id+1)); //Right Node
+        }
+
+        if(v.id - width > 0){
+            temp.add(new Vertex(v.id-width)); // TOp
+        }
+
+        if(v.id + width > (width*height)-1){
+            temp.add(new Vertex(v.id+width)); // Down
+        }
+
+
+
+
+
+        return temp;
     }
 
     /** 
